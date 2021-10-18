d8327ee0a8687d21c567c1d993c29f610cc598d4
Corentin <corentin.ach@gmail.com>
2021-10-18 18:54:27 +0200

    refactor: :art: React leaflet + mapbox

--- src/components/mainCard/index.tsx
+++ src/components/mainCard/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Paper, Typography, Box } from '@mui/material';
 
-const MainCard = () => {
-    return (
-    <Box width="300px" height="300px" sx={{ 
-        background: '#F0F0F0',
-        border: 0,
-        borderRadius: 10,
-    }}>
-        <Typography variant="h3" component="h2">
-            Bienvenue,
-        </Typography>
-        <Paper>
-            <Typography variant="h6" component="p">
-                Bienvenue,
-            </Typography>
-        </Paper>
-    </Box>
-)
-}
+const MainCard = () => (
+  <Box
+    width="300px"
+    height="300px"
+    sx={{
+      background: '#F0F0F0',
+      border: 0,
+      borderRadius: 10,
+    }}
+  >
+    <Typography variant="h3" component="h2">
+      Bienvenue,
+    </Typography>
+    <Paper>
+      <Typography variant="h6" component="p">
+        Bienvenue,
+      </Typography>
+    </Paper>
+  </Box>
+);
 
 export default MainCard;
