eb5b4e1ba66a7221628d55920a83a0005e37f5a1
corentin-ach <corentin.ach@gmail.com>
2021-10-18 18:54:27 +0200

    refactor: :art: React leaflet + mapbox

--- src/views/mapView/index.tsx
+++ src/views/mapView/index.tsx
@@ -1,30 +1,17 @@
-import React, { ReactElement } from "react";
-import GoogleMapReact from 'google-map-react';
-import MainCard from "../../components/mainCard";
+import React, { ReactElement } from 'react';
+import {
+  MapContainer, TileLayer,
+} from 'react-leaflet';
 
-const AnyReactComponent = (text: string) => <div>{text}</div>;
+const MapView = (): ReactElement => (
+  <div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://api.mapbox.com/styles/v1/corentin29/ckuvkg7qn260718qpfel4clh8/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA"
 
-const MapView = (): ReactElement => {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
+      />
+    </MapContainer>
+  </div>
+);
 
-
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <MainCard />
-      </GoogleMapReact>
-    </div>
-  );
-}
-  
-  export default MapView;
+export default MapView;
