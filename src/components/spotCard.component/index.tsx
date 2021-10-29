067f3fa8afc45ce189b45e38e4e585b62db07d90
Corentin <corentin.ach@gmail.com>
2021-10-29 22:48:04 +0200

    refactor: :art: Style spotCard

--- src/components/spotCard.component/index.tsx
+++ src/components/spotCard.component/index.tsx
@@ -4,23 +4,26 @@ import {
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import styles from './styles';
-import TempIcon from '../../assets/temp.svg';
-import FavIcon from '../../assets/fav.svg';
-import DropIcon from '../../assets/wi-raindrop.svg';
+import TempIcon from '../../assets/temp';
+import RainDropIcon from '../../assets/raindrop';
+import FavIcon from '../../assets/fav';
 
 const SpotCard = (): ReactElement => {
   const { t } = useTranslation(['translation']);
   const spotData = [
     {
-      icon: DropIcon,
+      id: 1,
+      icon: <RainDropIcon />,
       data: 'Excellent',
     },
     {
-      icon: TempIcon,
+      id: 2,
+      icon: <TempIcon />,
       data: '21',
     },
     {
-      icon: FavIcon,
+      id: 3,
+      icon: <FavIcon />,
       data: 'Favoris',
     },
 
@@ -28,33 +31,41 @@ const SpotCard = (): ReactElement => {
   return (
     <Box
       width="300px"
-      height="250px"
+      height="200px"
       sx={styles.mainCard}
     >
       <Typography sx={styles.spotTitle}>
-        La milady
+        La Milady
       </Typography>
-
-      <Grid>
-        {spotData.map((data) => (
-          <Box>
-            <Box>{data.icon}</Box>
-            <Typography>{data.data}</Typography>
-          </Box>
-        ))}
-      </Grid>
-
-      <Button
-        size="large"
-        sx={{
-          bgcolor: 'background.paper', textTransform: 'none', height: '50px', borderRadius: 2,
-        }}
+      <Box sx={{
+        justifyContent: 'space-between', display: 'flex', flexDirection: 'column', height: '100%', paddingTop: 2,
+      }}
       >
-        <Typography sx={styles.signalTitle}>
-          {t('translation:mapView.welcomeCard.signal')}
-        </Typography>
-      </Button>
+        <Grid container sx={{ justifyContent: 'space-around' }}>
+          {spotData.map((data) => (
+            <Box
+              key={data.id}
+              sx={{
+                bgcolor: 'background.paper', borderRadius: 2, height: 80, width: 90,
+              }}
+            >
+              <Box>{data.icon}</Box>
+              <Typography sx={{ color: 'text.primary' }}>{data.data}</Typography>
+            </Box>
+          ))}
+        </Grid>
 
+        <Button
+          size="large"
+          sx={{
+            bgcolor: 'background.paper', textTransform: 'none', height: '50px', borderRadius: 2, marginTop: 2,
+          }}
+        >
+          <Typography sx={styles.signalTitle}>
+            {t('translation:mapView.welcomeCard.signal')}
+          </Typography>
+        </Button>
+      </Box>
     </Box>
   );
 };
