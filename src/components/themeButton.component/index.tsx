2a2d8f14d26706f8f98bdecf21fc75504a0e0c27
corentin-ach <corentin.ach@gmail.com>
2021-10-30 12:36:28 +0200

    refactor: :art: dark mode opitmisations

--- src/components/themeButton.component/index.tsx
+++ src/components/themeButton.component/index.tsx
@@ -1,16 +1,21 @@
 import React, { ReactElement } from 'react';
 import { Box, IconButton } from '@mui/material';
-import { MdOutlineBrightness4 } from 'react-icons/md';
+import { MdOutlineBrightness4, MdOutlineWbSunny } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { toggleTheme } from '../../features/themeSlice';
 import styles from './styles';
 
-const ThemeButton = (): ReactElement => {
+interface Props {
+  isDark: boolean
+}
+
+const ThemeButton = (props: Props): ReactElement => {
   const dispatch = useDispatch();
+  const { isDark } = props;
   return (
     <Box sx={styles.toggleButton}>
       <IconButton onClick={() => dispatch(toggleTheme())} sx={{ bgcolor: 'background.default' }}>
-        <MdOutlineBrightness4 size={30} color="text.default" />
+        {isDark ? <MdOutlineWbSunny size={30} color="text.default" /> : <MdOutlineBrightness4 size={30} color="text.default" />}
       </IconButton>
     </Box>
   );
--- src/views/map.view/index.tsx
+++ src/views/map.view/index.tsx
@@ -3,32 +3,47 @@ import {
   MapContainer, TileLayer, Marker, ZoomControl,
 } from 'react-leaflet';
 import ThemeButton from '../../components/themeButton.component';
-import { lightMap } from '../../styles/theme';
+import { darkMap } from '../../styles/theme';
 import DropMarker from '../../components/dropMarker.component';
 import DataCards from '../../components/dataCards.component';
 
-const MapView = (): ReactElement => (
-  <div>
-    <MapContainer
-      center={[43.446782, -1.589371]}
-      zoom={13}
-      zoomControl={false}
-      attributionControl={false}
-      style={{
-        width: '100%',
-        height: '100vh',
-        zIndex: 2,
-      }}
-    >
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/corentin29/${lightMap}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA`}
-      />
-      <Marker position={[43.446782, -1.589371]} icon={DropMarker} />
-      <ZoomControl position="bottomright" />
-    </MapContainer>
-    <DataCards />
-    <ThemeButton />
-  </div>
-);
+interface Props {
+  isDark: boolean
+}
+
+const MapView = (props: Props): ReactElement => {
+  const { isDark } = props;
+  return (
+    <div>
+      <MapContainer
+        center={[43.446782, -1.589371]}
+        zoom={13}
+        zoomControl={false}
+        attributionControl={false}
+        style={{
+          width: '100%',
+          height: '100vh',
+          zIndex: 2,
+        }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/corentin29/${darkMap}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA`}
+        />
+        <Marker
+          position={[43.446782, -1.589371]}
+          icon={DropMarker}
+          eventHandlers={{
+            click: () => {
+              console.log('marker clicked');
+            },
+          }}
+        />
+        <ZoomControl position="bottomright" />
+      </MapContainer>
+      <DataCards />
+      <ThemeButton isDark={isDark} />
+    </div>
+  );
+};
 
 export default MapView;
