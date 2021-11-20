a58d3076d55889705da363991ffe72f2f26aaca7
corentin-ach <corentin.ach@gmail.com>
2021-11-20 20:09:11 +0100

    refactor: :sparkles: Loader when markers are fetched

---
+++ src/components/circularLoader.component/styles.ts
@@ -0,0 +1,14 @@
+const styles = {
+  loader: {
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    borderRadius: 2,
+    top: '30%',
+    right: '45%',
+    margin: 5,
+    zIndex: 10,
+  },
+} as const;
+
+export default styles;
