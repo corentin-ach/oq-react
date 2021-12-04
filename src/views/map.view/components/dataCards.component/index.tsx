6d204f8138915a5905390338b0930798d2fd4578
corentin-ach <corentin.ach@gmail.com>
2021-12-04 11:26:40 +0100

    refactor: :art: Snackbar position

--- src/views/map.view/components/dataCards.component/index.tsx
+++ src/views/map.view/components/dataCards.component/index.tsx
@@ -1,10 +1,15 @@
-import React, { ReactElement } from 'react';
-import { Box } from '@mui/material';
+import React, { ReactElement, useEffect, useState } from 'react';
+import {
+  Box, Snackbar, Alert, Slide, SlideProps,
+} from '@mui/material';
 import { motion } from 'framer-motion';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import SpotCard from '../spotCard.component';
 import MainCard from '../mainCard.component';
 import styles from './styles';
 import { Spot } from '../../../../features/setSpotSlice';
+import { RootState } from '../../../../app/store';
 
 interface Props {
   selectedSpot: Spot
@@ -12,7 +17,18 @@ interface Props {
 }
 
 const DataCards = (props: Props): ReactElement => {
+  const { t } = useTranslation(['translation']);
   const { selectedSpot, onClick } = props;
+  const { loading } = useSelector((state: RootState) => state.vote);
+  const [alert, setAlert] = useState(false);
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  const Transition = (slideProps: SlideProps): ReactElement => <Slide {...slideProps} direction="right" />;
+
+  useEffect(() => {
+    if (loading) {
+      setAlert(true);
+    }
+  }, [loading]);
   return (
     <Box sx={styles.dataCards}>
       <MainCard onClick={onClick} />
@@ -26,6 +42,16 @@ const DataCards = (props: Props): ReactElement => {
           <SpotCard selectedSpot={selectedSpot} />
         </motion.div>
       ) : null}
+      <Snackbar
+        open={alert}
+        autoHideDuration={6000}
+        onClose={() => setAlert(false)}
+        TransitionComponent={Transition}
+      >
+        <Alert severity="success" onClose={() => setAlert(false)} sx={{ width: '100%' }}>
+          {t('translation:mapView.spotCard.stackMessage')}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
