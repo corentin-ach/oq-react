c60ab5a1059d648c29cb7209d8a8c2ecc9942377
corentin-ach <corentin.ach@gmail.com>
2021-12-05 20:55:11 +0100

    feat: :rocket: Introduction Stats Card

---
+++ src/views/content.view/components/stats.component/styles.ts
@@ -0,0 +1,32 @@
+const styles = {
+  statsTitle: {
+    color: 'text.primary',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginLeft: 2,
+  },
+  dataSubtitle: {
+    color: 'text.primary',
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginLeft: 0.5,
+  },
+  data: {
+    color: 'text.primary',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: 'text.primary',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+  time: {
+    fontSize: 12,
+    marginBottom: 2,
+    marginLeft: 2,
+  },
+} as const;
+
+export default styles;
--- src/views/map.view/index.tsx
+++ src/views/map.view/index.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement, useEffect } from 'react';
 import {
-  MapContainer, useMap,
+  MapContainer,
 } from 'react-leaflet';
-import L from 'leaflet';
 import { useDispatch, useSelector } from 'react-redux';
 import DataCards from './components/dataCards.component';
 import MarkersCluster from './components/markersCluster.component';
@@ -10,25 +9,12 @@ import { RootState } from '../../app/store';
 import { getSpots } from '../../features/getSpotsSlice';
 import CircularLoader from '../../components/circularLoader.component';
 import LocationMarker from './components/locationMarker.component';
-import { darkMap, lightMap } from '../../styles/theme';
+import TileLayer from './components/tileLayer.component';
 
 interface Props {
   isDark: boolean
   onIntroClick: () => void;
 }
-
-const TileLayer = ({ isDark }: any) => {
-  const map = useMap();
-  const darkLayer = L.tileLayer(
-    `https://api.mapbox.com/styles/v1/corentin29/${darkMap}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA`,
-  );
-
-  const lightLayer = L.tileLayer(
-    `https://api.mapbox.com/styles/v1/corentin29/${lightMap}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA`,
-  );
-  map.addLayer(isDark ? darkLayer : lightLayer);
-  return null;
-};
 
 const MapView = (props: Props): ReactElement => {
   const { isDark, onIntroClick } = props;
