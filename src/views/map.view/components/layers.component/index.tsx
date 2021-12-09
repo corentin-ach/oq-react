8a2f732f685ffee8842b0c84dc0b58845638fe59
corentin-ach <corentin.ach@gmail.com>
2021-12-09 15:02:25 +0100

    refactor: :zap: Markers with Geojson

---
+++ src/views/map.view/components/layers.component/index.tsx
@@ -0,0 +1,37 @@
+export const clusterLayer: any = {
+  id: 'clusters',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  paint: {
+    'circle-color': ['step', ['get', 'point_count'], '#5DADEC', 100, '#5DADEC', 750, '#5DADEC'],
+    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+    'circle-blur': 0.6,
+  },
+};
+
+export const clusterCountLayer: any = {
+  id: 'cluster-count',
+  type: 'symbol',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  layout: {
+    'text-field': '{point_count_abbreviated}',
+    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+    'text-size': 12,
+  },
+  paint: {
+    'text-color': '#ffffff',
+  },
+};
+
+export const unclusteredPointLayer: any = {
+  id: 'unclustered-point',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['!', ['has', 'point_count']],
+  paint: {
+    'circle-color': '#5DADEC',
+    'circle-radius': 5,
+  },
+};
--- src/views/map.view/index.tsx
+++ src/views/map.view/index.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { ReactElement, useEffect } from 'react';
-import MapGL from 'react-map-gl';
-
+import React, {
+  ReactElement, useEffect, useRef, useState,
+} from 'react';
+import MapGL, { Layer, Source } from 'react-map-gl';
 import { useDispatch, useSelector } from 'react-redux';
+import { GeoJsonProperties } from 'geojson';
 import DataCards from './components/dataCards.component';
 import { RootState } from '../../app/store';
 import { getSpots } from '../../features/getSpotsSlice';
 import CircularLoader from '../../components/circularLoader.component';
 import { darkMap, lightMap } from '../../styles/theme';
+import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from './components/layers.component';
 
 interface Props {
   isDark: boolean
@@ -17,9 +20,10 @@ interface Props {
 const MapView = (props: Props): ReactElement => {
   const { isDark, onIntroClick } = props;
   const dispatch = useDispatch();
-  const { loading } = useSelector((state: RootState) => state.spots);
-  // const { location } = useSelector((state: RootState) => state.location);
+  const { spots, loading } = useSelector((state: RootState) => state.spots);
+  const [markers, setMarkers]: any = useState({});
   const { spot } = useSelector((state: RootState) => state.spot);
+  const mapRef: any = useRef(null);
 
   useEffect(() => { dispatch(getSpots()); }, []);
 
@@ -29,6 +33,25 @@ const MapView = (props: Props): ReactElement => {
     zoom: 6,
   });
 
+  const geojson: GeoJsonProperties = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  useEffect(() => {
+    spots.map((element) => geojson.features.push({
+      type: 'Feature',
+      properties: {
+        id: element.id,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [element.coords[1], element.coords[0]],
+      },
+    }));
+    setMarkers(geojson);
+  }, [spots]);
+
   return (
     <div>
       <MapGL
@@ -37,8 +60,22 @@ const MapView = (props: Props): ReactElement => {
         height="100vh"
         mapStyle={isDark ? darkMap : lightMap}
         onViewportChange={setViewport}
+        ref={mapRef}
         mapboxApiAccessToken="pk.eyJ1IjoiY29yZW50aW4yOSIsImEiOiJja3V3dmgxOG0wMTdpMnZsOGs2OGU4eDQzIn0.p3UORX0_zEWs7XpxBBWMHA"
-      />
+      >
+        <Source
+          id="earthquakes"
+          type="geojson"
+          data={markers}
+          cluster
+          clusterMaxZoom={14}
+          clusterRadius={50}
+        >
+          <Layer {...clusterLayer} />
+          <Layer {...clusterCountLayer} />
+          <Layer {...unclusteredPointLayer} />
+        </Source>
+      </MapGL>
       <DataCards selectedSpot={spot} onClick={onIntroClick} />
       {loading ? <CircularLoader /> : null}
     </div>
